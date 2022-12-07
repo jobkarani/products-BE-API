fed0b115441ee6c13cfd0c23124e32f47e5f1a5a
Job Karani <jobkarani084j@gmail.com>
2022-12-07 02:03:41 -0800

    creates productpage method

--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from app.models import *
 
 # Create your views here.
+
+def productsPage(request, category_slug=None,product_slug=None):
+    categories = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+    context = {
+        'product_count':product_count,
+    }
+    return products
 
 def product_detail(request, category_slug, product_slug):
 
