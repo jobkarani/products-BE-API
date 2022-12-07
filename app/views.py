24d0995eab73712a058c50745739d34902d2a460
Job Karani <jobkarani084j@gmail.com>
2022-12-07 01:59:58 -0800

    creates product details method

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from app.models import *
+
 # Create your views here.
+
+def product_detail(request, category_slug, product_slug):
+
+    try:
+        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        products = Product.objects.all().filter(is_available=True)
+
+    except Exception as e:
+        raise e
+
+    context = {
+    'single_product': single_product,
+    'products':products,
+    }
+
+    return single_product
