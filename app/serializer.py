b995a1d1113441e11caf2244d6b5af2ef46f1f8c
Job Karani <jobkarani084j@gmail.com>
2022-12-12 18:26:30 +0300

    adds id field to the serializer class

--- app/serializer.py
+++ app/serializer.py
@@ -5,13 +5,13 @@ from .models import *
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name', 'slug']
+        fields = ['id', 'name', 'slug']
 
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name')
     category_slug = serializers.CharField(source='category.slug')
     class Meta:
         model = Product
-        fields = ['name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug']
+        fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug']
 
     
