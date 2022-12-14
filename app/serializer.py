41553e181eb96e3286ac48e0e1bbb24cf3d9c98a
Job Karani <jobkarani084j@gmail.com>
2022-12-15 00:15:21 +0300

    adds firld category_id to the product serializer class

--- app/serializer.py
+++ app/serializer.py
@@ -10,8 +10,9 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name')
     category_slug = serializers.CharField(source='category.slug')
+    category_id = serializers.IntegerField(source='category.id')
     class Meta:
         model = Product
-        fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug']
+        fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug','category_id']
 
     
