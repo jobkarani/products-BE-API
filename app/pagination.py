f29822efbf338314eaac413f1b1e833b9f4d068a
Job Karani <jobkarani084j@gmail.com>
2022-12-15 17:32:49 +0300

    changes page size query param to id

--- app/pagination.py
+++ app/pagination.py
@@ -2,6 +2,6 @@ from rest_framework import pagination
 
 class CustomPageNumberPagination(pagination.PageNumberPagination):
     page_size = 1
-    page_size_query_param = 'count'
+    page_size_query_param = 'id'
     max_page_size = 5
     page_query_param = 'p'
