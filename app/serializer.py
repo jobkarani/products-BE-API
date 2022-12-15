8a3b62af21019d7eb7ecc34ce6256e0584f62183
Job Karani <jobkarani084j@gmail.com>
2022-12-15 09:14:30 +0300

    modifies getcategoryproducts method

--- app/serializer.py
+++ app/serializer.py
@@ -8,12 +8,12 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'slug']
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True, read_only=True)
+    # category = CategorySerializer(many=True, read_only=True)
     category_name = serializers.CharField(source='category.name')
     category_slug = serializers.CharField(source='category.slug')
     category_id = serializers.IntegerField(source='category.id')
     class Meta:
         model = Product
-        fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug','category_id','category']
+        fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug','category_id']
 
     
