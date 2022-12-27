3d4f9a03259769b4449bb3ac93d9110038eacf73
Job Karani <jobkarani084j@gmail.com>
2022-12-27 20:34:27 +0300

    dashboard

--- app/views.py
+++ app/views.py
@@ -44,40 +44,6 @@ def productsPage(request, category_slug=None):
     }
     return render(request, 'products.html', context)
 
-def product_detail(request, category_slug, product_slug):
-
-    try:
-        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
-        products = Product.objects.all().filter(is_available=True)
-
-    except Exception as e:
-        raise e
-
-    context = {
-    'single_product': single_product,
-    'products':products,
-    }
-
-    return render(request, 'productDetails.html', context)
-
-def search(request):
-    products = 0
-    product_count = 0
-    if 'keyword' in request.GET:
-        keyword=request.GET['keyword']
-        if keyword:
-            products= Product.objects.order_by('-name').filter(Q(description__icontains=keyword) | Q(name__icontains=keyword))
-            product_count = products.count()
-        elif keyword != keyword :
-
-            return HttpResponse('Ooops no products found with that keyword :(  Try another Keyword :)')
-
-    ctx={
-        'products':products,
-        'product_count':product_count,
-    }
-    return render(request,'products.html', ctx)
-
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
